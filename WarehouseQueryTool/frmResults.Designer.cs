523eb9405c2cc3d6c3d972aa29ca945d39dd617a
Christopher Mills <cmills@edge-ka.com>
2021-02-16 15:05:28 +0000

    Filtering and Export to Excel

--- WarehouseQueryTool/frmResults.Designer.cs
+++ WarehouseQueryTool/frmResults.Designer.cs
@@ -31,6 +31,16 @@ namespace WarehouseQueryTool
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmResults));
             this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtExpt = new System.Windows.Forms.TextBox();
+            this.txtProcess = new System.Windows.Forms.TextBox();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.lblExpt = new System.Windows.Forms.Label();
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.lblMethod = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,16 +48,114 @@ namespace WarehouseQueryTool
             // 
             this.dgvResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvResults.Location = new System.Drawing.Point(12, 12);
+            this.dgvResults.Location = new System.Drawing.Point(12, 77);
             this.dgvResults.Name = "dgvResults";
             this.dgvResults.Size = new System.Drawing.Size(1374, 669);
             this.dgvResults.TabIndex = 0;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(440, 41);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 1;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(521, 41);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear Filter";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1311, 752);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtExpt
+            // 
+            this.txtExpt.Location = new System.Drawing.Point(12, 44);
+            this.txtExpt.Name = "txtExpt";
+            this.txtExpt.Size = new System.Drawing.Size(138, 20);
+            this.txtExpt.TabIndex = 4;
+            // 
+            // txtProcess
+            // 
+            this.txtProcess.Location = new System.Drawing.Point(154, 44);
+            this.txtProcess.Name = "txtProcess";
+            this.txtProcess.Size = new System.Drawing.Size(138, 20);
+            this.txtProcess.TabIndex = 5;
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Location = new System.Drawing.Point(296, 44);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.Size = new System.Drawing.Size(138, 20);
+            this.txtMethod.TabIndex = 6;
+            // 
+            // lblExpt
+            // 
+            this.lblExpt.AutoSize = true;
+            this.lblExpt.Location = new System.Drawing.Point(50, 28);
+            this.lblExpt.Name = "lblExpt";
+            this.lblExpt.Size = new System.Drawing.Size(59, 13);
+            this.lblExpt.TabIndex = 7;
+            this.lblExpt.Text = "Expt Name";
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Location = new System.Drawing.Point(185, 28);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(76, 13);
+            this.lblProcess.TabIndex = 8;
+            this.lblProcess.Text = "Process Name";
+            // 
+            // lblMethod
+            // 
+            this.lblMethod.AutoSize = true;
+            this.lblMethod.Location = new System.Drawing.Point(329, 28);
+            this.lblMethod.Name = "lblMethod";
+            this.lblMethod.Size = new System.Drawing.Size(74, 13);
+            this.lblMethod.TabIndex = 9;
+            this.lblMethod.Text = "Method Name";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(1230, 752);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 10;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
             // frmResults
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1398, 693);
+            this.ClientSize = new System.Drawing.Size(1397, 785);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lblMethod);
+            this.Controls.Add(this.lblProcess);
+            this.Controls.Add(this.lblExpt);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.txtProcess);
+            this.Controls.Add(this.txtExpt);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFilter);
             this.Controls.Add(this.dgvResults);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmResults";
@@ -55,11 +163,22 @@ namespace WarehouseQueryTool
             this.Text = "Query Results";
             ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtExpt;
+        private System.Windows.Forms.TextBox txtProcess;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Label lblExpt;
+        private System.Windows.Forms.Label lblProcess;
+        private System.Windows.Forms.Label lblMethod;
+        private System.Windows.Forms.Button btnExport;
     }
 }
--- WarehouseQueryTool/frmResults.cs
+++ WarehouseQueryTool/frmResults.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WarehouseQueryTool
 {
@@ -109,6 +111,128 @@ namespace WarehouseQueryTool
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            string rowFilter = "";
+            if (txtExpt.Text != "")
+            {
+                rowFilter += string.Format("[{0}] LIKE '%{1}%'", "Expt_Name", txtExpt.Text);
+            }
+            if (txtProcess.Text != "")
+            {
+                if (rowFilter.Length == 0)
+                {
+                    rowFilter += string.Format("[{0}] LIKE '%{1}%'", "Process_Ver_Name", txtProcess.Text);
+                }
+                else
+                {
+                    rowFilter += string.Format("AND [{0}] LIKE '%{1}%'", "Process_Ver_Name", txtProcess.Text);
+                }
+            }
+            if (txtMethod.Text != "")
+            {
+                if (rowFilter.Length == 0)
+                {
+                    rowFilter += string.Format("[{0}] LIKE '%{1}%'", "Method_Name", txtMethod.Text);
+                }
+                else
+                {
+                    rowFilter += string.Format("AND [{0}] LIKE '%{1}%'", "Method_Name", txtMethod.Text);
+                }
+            }
+
+            //rowFilter += string.Format(" OR [{0}] = '{1}'", columnName, additionalFilterValue);
+            (dgvResults.DataSource as DataTable).DefaultView.RowFilter = rowFilter;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            string rowFilter = string.Empty;
+            (dgvResults.DataSource as DataTable).DefaultView.RowFilter = rowFilter;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+        
+        private void copyAlltoClipboard()
+        {
+             dgvResults.SelectAll();
+            DataObject dataObj = dgvResults.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "WarehouseQuery-" + System.DateTime.Now.ToString().Replace('/', '-').Replace(':', '-') + ".xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                dgvResults.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+                // Copy DataGridView results to clipboard
+                copyAlltoClipboard();
+
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Application xlexcel = new Excel.Application();
+
+                xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
+                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                // Paste clipboard results to worksheet range
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                //For some reason column A is always blank in the worksheet. ¯\_(ツ)_ /¯
+                // Delete blank column A and select cell A1
+                Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                delRng.Delete(Type.Missing);
+                xlWorkSheet.get_Range("A1").Select();
+
+                // Save the excel file under the captured location from the SaveFileDialog
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlexcel.DisplayAlerts = true;
+                xlWorkBook.Close(true, misValue, misValue);
+                xlexcel.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlexcel);
+
+                // Clear Clipboard and DataGridView selection
+                Clipboard.Clear();
+                dgvResults.ClearSelection();
+
+                // Open the newly saved excel file
+                if (File.Exists(sfd.FileName))
+                    System.Diagnostics.Process.Start(sfd.FileName);
             }
         }
     }
